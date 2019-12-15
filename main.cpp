16c7b93c3c0a91dc24a446550c7ad5e3e0a562e3
irinapashkevich <58890126+irinapashkevich@users.noreply.github.com>
2019-12-15 13:31:09 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -116,53 +116,74 @@ void delete_tree (tree_element* root)
     delete (root);
 }
 
-int list_of_tree(struct tree_element* root)
+
+int max_of_tree(struct tree_element* root)
 {
-    if ((root->left==NULL) && (root->right==NULL))
+    int a;
+    if (root->right!=NULL)
     {
-        return root->value;
-    }
-    if (root->right==NULL)
-    {
-        int a=root->left->value;
-        delete (root->left);
-        root->left=NULL;
-        root->height=1;
-        return a;
-    }
-    if (root->left==NULL)
-    {
-        int a=root->right->value;
-        delete (root->right);
-        root->right=NULL;
-        root->height=1;
-        return a;
-    }
-    if (root->left->value>root->right->value+1)
-    {
-        int a=list_of_tree(root->left);
-        if (a==root->left->value)
+        a=max_of_tree(root->right);
+        if (root->right->value==a)
         {
-            delete(root->left);
-            root->left=NULL;
+            delete (root->right);
+            root->right=NULL;
         }
-        root->height=max(height(root->left), height(root->right))+1;
-        return a;
+        if (height(root->left)>height(root->right)+1)
+        {
+            if (height(root->left->right)>height(root->left->left))
+            {
+                root->left=left_rotate(root->left);
+            }
+            root=right_rotate(root);
+        }
     }
     else
     {
-         int a=list_of_tree(root->right);
-         if (a==root->right->value)
-         {
-             delete(root->right);
-             root->right=NULL;
-         }
-         root->height=max(height(root->left), height(root->right))+1;
-         return a;
+        a=root->value;
+        if (root->left!=NULL)
+        {
+            root->value=root->left->value;
+            delete (root->left);
+            root->left=NULL;
+        }
     }
+    return a;
 }
 
-tree_element *delete_tree_element (tree_element* root, int value)
+int min_of_tree(struct tree_element* root)
+{
+    int a;
+    if (root->left!=NULL)
+    {
+        a=min_of_tree(root->right);
+        if (root->left->value==a)
+        {
+            delete (root->left);
+            root->left=NULL;
+        }
+        if (height(root->right)>height(root->left)+1)
+        {
+            if (height(root->right->left)>height(root->right->right))
+            {
+                root->right=right_rotate(root->right);
+            }
+            root=left_rotate(root);
+        }
+    }
+    else
+    {
+        a=root->value;
+        if (root->right!=NULL)
+        {
+            root->value=root->right->value;
+            delete (root->right);
+            root->right=NULL;
+        }
+    }
+    return a;
+}
+
+tree_element *delete_tree_element (struct tree_element* root, int value)
 {
     if (root->value==value)
     {
@@ -193,8 +214,25 @@ tree_element *delete_tree_element (tree_element* root, int value)
         }
         else
         {
-            root->value=list_of_tree(root);
-            root->height=max(height(root->right), height(root->left));
+            if (root->left->height>root->right->height)
+            {
+               root->value=max_of_tree(root->left);
+               if (root->left->value==root->value)
+               {
+                   delete(root->left);
+                   root->left=NULL;
+               }
+            }
+            else
+            {
+               root->value=min_of_tree(root->right);
+               if (root->right->height==root->value);
+               {
+                   delete(root->right);
+                   root->right=NULL;
+               }
+            }
+            root->height=max(height(root->right), height(root->left))+1;
             return root;
         }
     }
@@ -203,6 +241,7 @@ tree_element *delete_tree_element (tree_element* root, int value)
         if (value>root->value)
         {
             root->right=delete_tree_element(root->right, value);
+            root->height=max(height(root->right), height(root->left))+1;
             if (height(root->left)>height(root->right)+1)
             {
                 if (height(root->left->right)>height(root->left->left))
@@ -215,6 +254,7 @@ tree_element *delete_tree_element (tree_element* root, int value)
         if (value<root->value)
         {
             root->left=delete_tree_element(root->left, value);
+            root->height=max(height(root->right), height(root->left))+1;
             if (height(root->right)>height(root->left)+1);
             {
                 if (height(root->right->left)>height(root->right->right))
@@ -253,8 +293,13 @@ int main()
     print_tree(p);
     cout<<p->height<<endl;
     cout<<endl;
-    cin>>a;
-    p=delete_tree_element(p, a);
+    cout<<"kol-vo delete=";
+    cin>>n;
+    for (int i=0; i<n; i++)
+    {
+       cin>>a;
+       p=delete_tree_element(p, a);
+    }
     print_tree(p);
     cout<<p->height<<endl;
     delete_tree(p);
